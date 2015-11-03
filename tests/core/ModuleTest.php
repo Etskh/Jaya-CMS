a55daed33d60fcb2033584670b9696c5aa86b232
Etskh <etskh@hotmail.com>
2015-11-03 14:27:43 -0800

    Adding basic tests for PHPUnit

---
+++ tests/core/ModuleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class ModuleTest extends PHPUnit_Framework_TestCase
+{
+	/*
+	 * @test
+	 */
+	public function test_ModuleLoader () {
+
+		//$module = new Module();
+
+		$this->assertEquals( 1, intval('1') );
+	}
+}
